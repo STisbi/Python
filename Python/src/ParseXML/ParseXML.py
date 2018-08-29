e4094305a2ae9869850be85725ba19e04052c2b7
STisb <STisb@gmail.com>
2018-08-29 10:15:29 -0500

    Modify and Namespace functions added. XML file namespace.xml added.

--- Python/src/ParseXML/ParseXML.py
+++ Python/src/ParseXML/ParseXML.py
@@ -2,6 +2,8 @@ import xml.etree.ElementTree as ET
 
 
 #######################################################
+# Name: Parse
+#
 # Description: Gets the tree structure
 #                  and calls the other methods
 #              Comment in/out the functions as needed
@@ -12,11 +14,33 @@ def Parse(script):
     tree = ET.parse(script)
     root = tree.getroot()
     
-    PrintTA(root)
-    Find(root)
+    #PrintTA(root)
+    #Find(root)
+    #Modify(tree)
+    Namespace(root)
     
 
 #######################################################
+# Name: PrintTA
+#
+# Description: Prints all tags and attributes         
+#
+# Parameters:  The XML root
+#######################################################
+def PrintTA(root):
+    print("\nPrintTA\n")
+    
+    for data in root:
+        print("Data Tag: ", data.tag)
+        print("Data Attribute: ", data.attrib)
+        for country in data:
+            print("Country Tag: ", country.tag)
+            print("Country Attribute : ", country.attrib)
+
+
+#######################################################
+# Name: Find
+#
 # Description: Variations on finding parts of the XML
 #
 # Parameters:  The XML file
@@ -36,36 +60,69 @@ def Find(root):
     for country in root.findall('country'):
         
         # Gets the first child 'rank'
-        rank = country.find('rank').text
+        rankT = country.find('rank').text
+        
+        # Gets the first child 'rank''s attribute
+        rankA = country.find('rank').get('update')
         
         # .get prints the attribute
         name = country.get('name')
         
-        print(name, rank)
+        print(name, rankA, rankT)
         
 
+#######################################################
+# Name: Modify
+#
+# Description: Modifies the XML file
+#
+# Parameters:  The XML tree
+#######################################################
+def Modify(tree):
+        print("\nModify\n")
+        
+        root= tree.getroot()
+        
+        # Iterate through element rank
+        for rank in root.iter("rank"):
+            
+            # Adds/Subs 999 from its value
+            newRank = int(rank.text) + 999
+            rank.text = str(newRank)
+            
+            # Haven't found documentation on what the params for .set are
+            rank.set("update", "yes")
+            
+        tree.write("script.xml")
+            
 
 #######################################################
-# Description: Prints all tags and attributes         
+# Name: Namespace
+#
+# Description: Find but with a namespaced XML
 #
 # Parameters:  The XML root
 #######################################################
-def PrintTA(root):
-    print("\nPrintTA\n")
+def Namespace(root):
+    print("\nNamespace\n")
     
-    for data in root:
-        print("Data Tag: ", data.tag)
-        print("Data Attribute: ", data.attrib)
-        for country in data:
-            print("Country Tag: ", country.tag)
-            print("Country Attribute : ", country.attrib)
+    # Dictionary using the namespaces from the XML 
+    namespace = {"fiction" : "http://characters.example.com",
+                 "xlms" : "http://people.example.com"}
+    
+    for actor in root.findall("xlms:actor", namespace):
+        name = actor.find("xlms:name", namespace)
+        print(name.text)
+        
+        for character in actor.findall("fiction:character", namespace):
+            print(character.text)
 
 
 #######################################################
 #                        MAIN                         #
 #######################################################
 def main():
-    file = "script.xml"
+    file = "namespace.xml"
     Parse(file)
 
 if __name__ == "__main__":
