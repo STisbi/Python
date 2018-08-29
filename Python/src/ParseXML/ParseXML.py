a8dad9cc3d3adf68e433c8475c4f3267eb6f68c2
STisb <STisb@gmail.com>
2018-08-29 09:19:36 -0500

    Added functions PrintTA and Find. Modified the XML file. Used https://docs.python.org/2/library/xml.etree.elementtree.html for help

--- Python/src/ParseXML/ParseXML.py
+++ Python/src/ParseXML/ParseXML.py
@@ -1,24 +1,64 @@
 import xml.etree.ElementTree as ET
 
+
+#######################################################
+# Description: Gets the tree structure
+#                  and calls the other methods
+#              Comment in/out the functions as needed
+#
+# Parameters:  The XML file
+#######################################################
 def Parse(script):
     tree = ET.parse(script)
     root = tree.getroot()
+    
+    PrintTA(root)
+    Find(root)
+    
 
-    for child in root:
-        gotStuff = DoStuff(str(child.text))
-        print(gotStuff)
+#######################################################
+# Description: Variations on finding parts of the XML
+#
+# Parameters:  The XML file
+#######################################################
+def Find(root):
+    print("\nFind\n")
+    
+    # Iterate through all elements with tag country
+    for country in root.iter('country'):
+        print(country.attrib)
+        
+    # Iterate through all elements with tag neighbor
+    for neighbor in root.iter('neighbor'):
+        print(neighbor.attrib)
+        
+    # Find all country elements
+    for country in root.findall('country'):
+        
+        # Gets the first child 'rank'
+        rank = country.find('rank').text
+        
+        # .get prints the attribute
+        name = country.get('name')
+        
+        print(name, rank)
+        
 
 
-def DoStuff(text):
-    stuff = text.split()
-
-    # Go through the list
-    for x in stuff:
-        return x
-
-
-
-
+#######################################################
+# Description: Prints all tags and attributes         
+#
+# Parameters:  The XML root
+#######################################################
+def PrintTA(root):
+    print("\nPrintTA\n")
+    
+    for data in root:
+        print("Data Tag: ", data.tag)
+        print("Data Attribute: ", data.attrib)
+        for country in data:
+            print("Country Tag: ", country.tag)
+            print("Country Attribute : ", country.attrib)
 
 
 #######################################################
