da768be820a9f2746796d0522cf1d798b0998b67
STisb <STisb@gmail.com>
2018-09-22 20:03:14 -0500

    Added Quick_Sort.py, added doxygen headers for all functions in it and in Merge_Sort.

--- Python/src/Algorithms/Quick_Sort.py
+++ Python/src/Algorithms/Quick_Sort.py
@@ -3,80 +3,58 @@ from random import randint
 import sys
 
 
-# @brief The driver for MergeSort, partitions the array then recusively sorts the left and right subarrays
+# @brief The driver for QuickSort, partitions the array then recusively sorts the left and right subarrays
 #
 # @param array The array to be worked on
 # @param start The index to start a subarray
 # @param end   The index to end a subarray
 #
 # @return A sorted integer array of given size
-def MergeSort(array, start, end):
+def QuickSort(array, start, end):
     
     if(start < end):
-        # Calculate halfway point of array
-        half = int((start + end) / 2) 
+        # Get the half way point
+        half = Partition(array, start, end)
         
-        # Divide left half of array
-        MergeSort(array, start, half)
+        # Sort the left array
+        QuickSort(array, start, half - 1)
         
-        # Divide right half of array
-        MergeSort(array, half + 1, end)
-        
-        # Merge the two halves
-        Merge(array, start, half, end)
+        # Sort the right array
+        QuickSort(array, half + 1, end)
         
     return array
 
 
-# @brief Iteratively merges two sorted arrays
+# @brief Iteratively swaps two elements compared to a pivot element
 #
 # @param array The array to be worked on
-# @param start The index to start a merge
-# @param half  The index for the half way point of the array
-# @param end   The index to end a merge
+# @param start The index to start the swap
+# @param end   The index to end the swap
 #
 # @return An semi-sorted array with elements greater than the pivot the right of it and elements less than the pivot to the left of it
-def Merge(array, start, half, end):
-    # Calculate the size of each half
-    # Plus 1 to accommodate uneven sized arrays
-    # Halving an uneven array gives .5, converting to int truncates that
-    # Ex. Array size 3, half is 1.5, int truncates to 1
-    nLeft = half - start + 1
-    nRght = end - half
+def Partition(array, start, end):
+    pivot = array[end]
+    left = start - 1
     
-    # Create the arrays and add one for the sentinel values
-    lArray = [None]* int((nLeft + 1))
-    rArray = [None]* int((nRght + 1))
+    for right in range(start, end):
+        if(array[right] <= pivot):
+            left += 1
+            
+            # Swap elements            
+            array[left], array[right] = array[right], array[left]
     
-    # Populate the arrays, again plus 1 handles the uneven sizes
-    lArray = array[start:half + 1]
-    rArray = array[half + 1: end + 1]
+    # Swap with the pivot, now between both halves
+    array[left + 1], array[end] = array[end], array[left + 1]
     
-    # Sentinel values
-    lArray.append(sys.maxsize)
-    rArray.append(sys.maxsize)
-    
-    #print('lArray:', lArray, 'rArray', rArray)
-    
-    lIndex = 0
-    rIndex = 0
-    
-    # Plus 1 ensures if both arrays have size 1, both values get added to array
-    for index in range(start, end + 1):
-        if(lArray[lIndex] < rArray[rIndex]):
-            array[index] = lArray[lIndex]
-            lIndex += 1
-        else:
-            array[index] = rArray[rIndex]
-            rIndex += 1
-    
+    return (left + 1)
+
 
 # @brief Creates an unsorted array of given size.
 #
 # @param size An optional parameter that specifies the size of the array
 #
-# @return An unsorted integer array of given size    
-def createArray(size = 1000):
+# @return An unsorted integer array of given size
+def createArray(size = 20):
     # The Numpy library can create a random array of size x in one line
     #    but it doesn't come installed with python
     
@@ -120,13 +98,14 @@ def main(argv):
         # createArray takes an optional - size of array to be created - argument
         #     Default is 1000,
         inputArray = createArray(inputArray[0])
+        
+    print("The array of size", len(inputArray), "is:", inputArray)
     
     # Get the execution time of MergeSort
     start = timer()
-    sortedArray = MergeSort(inputArray, 0, len(inputArray) - 1)
+    sortedArray = QuickSort(inputArray, 0, len(inputArray) - 1)
     end = timer()
     
-    print("The array of size", len(inputArray), "is:", inputArray)
     print("The MergeSort sorted array is:", sortedArray)
     print("The execution time is", end - start, "seconds.")
     
