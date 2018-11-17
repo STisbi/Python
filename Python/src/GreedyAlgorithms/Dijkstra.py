d7c025172f708e3495ddea160da410fdfaa8125e
Seth <STisb@gmail.com>
2018-11-16 21:12:33 -0600

    Dijkstra's implemented. Prints dictionary with distances and parents.
    Still to be beautify output to more cleaner, leaner, meaner look.

--- Python/src/GreedyAlgorithms/Dijkstra.py
+++ Python/src/GreedyAlgorithms/Dijkstra.py
@@ -7,11 +7,85 @@ global edgeWeights
 # @brief The main algorithm for finding the shortest path
 #
 # @return The shortest path
-def Dijkstra():
+def Dijkstra(source):
+    # Giant value to simulate infinity
+    infinity = sys.maxsize
+    
     vertexList = CreateVertexList()
+    vertexQueue = vertexList
+    vertexQueue.remove(source)
+    vertexQueue.insert(0, source)
     adjMatrix  = CreateAdjacencyMatrix(vertexList)
     
+    #DEBUGprint("Vertices:", vertexList)
     PrintAdjacenyMatrix(adjMatrix)
+    
+    # The table for keeping track of total distances
+    distances = {}
+    
+    # Table for keeping track of a vertices' parents
+    parents = {}
+    
+    # Iterate through every vertex
+    for vertex in vertexList:
+        # Initialize all vertices to no parents
+        parents[vertex]    = ''
+        
+        # The source is the starting point so its value needs to be 0
+        if vertex == source:
+            distances[vertex] = 0
+        else:
+            distances[vertex] = infinity
+     
+    #DEBUGprint("Distances:", distances, "\nParent:   ", parents)
+    position = 0
+    
+    
+    for index, vertex in enumerate(vertexQueue):
+        if index != 0:
+            vertexQueue.remove(vertexQueue[index - 1])
+        for edge in edgeWeights:
+            if (vertex in edge) and (vertex in vertexQueue):
+                if edge[0] == vertex:
+                    position = 1
+                else:
+                    position = 0
+                
+                path = edge[2] + distances[vertex]
+                
+                #print("V:", vertex, "\nQ:", vertexQueue, "\nE:", edge, "\nP:", path)
+                #print("Distances:", distances, "\nParent:   ", parents)
+            
+                if path < distances[edge[position]]:
+                    distances[edge[position]] = path
+                    parents[edge[position]] = vertex
+                    #print("Distances:", distances, "\nParent:   ", parents)
+        
+        # The next vertex to be considered has to be the one with the smallest path value
+        MaintainPriorityQueue(vertexQueue, distances, source)
+        
+    print("Distances:", distances, "\nParents:  ", parents)
+
+
+# @brief Maintains the priority queue
+def MaintainPriorityQueue(vertexQueue, distances, source):
+    minimumValue = sys.maxsize
+    priorityVertex = ''
+    
+    # Iterate through only vertices in the queue
+    for vertex in vertexQueue:
+        # Find vertex with the smallest distance priorityVertex and that is not the source
+        if (distances.get(vertex) < minimumValue) and (vertex != source):
+            minimumValue = distances.get(vertex)
+            priorityVertex = vertex
+    
+    #DEBUGprint("M:", priorityVertex, "\nM:", vertexQueue)
+    
+    # Make sure that priorityVertex is in the queue
+    if priorityVertex in vertexQueue:
+        # TODO: I'm sure there is a cleaner way to reorder the list
+        vertexQueue.remove(priorityVertex)
+        vertexQueue.insert(1, priorityVertex)
 
 
 # @brief Creates the adjaceny matrix
@@ -114,25 +188,23 @@ def main(argv):
     global edgeWeights
     
     if(not argv):
-        UserInput()
-        edgeWeights = [[ 97,  98, 1],
-                       [ 97,  99, 6],
-                       [ 98, 100, 2],
-                       [ 98, 101, 7],
-                       [ 99, 101, 1],
-                       [100, 101, 1]]
+        #UserInput()
+        
+        edgeWeights = [[ 'a', 'b', 10],
+                       [ 'a', 'c', 20],
+                       [ 'b', 'c',  5],
+                       [ 'b', 'd', 16],
+                       [ 'c', 'd', 20]]
     else:
-        # 97 is ascii for 'a'
-        edgeWeights = [[ 97,  98, 1],
-                       [ 97,  99, 6],
-                       [ 98, 100, 2],
-                       [ 98, 101, 7],
-                       [ 99, 101, 1],
-                       [100, 101, 1]]
+        edgeWeights = [[ 'a', 'b', 10],
+                       [ 'a', 'c', 20],
+                       [ 'b', 'c',  5],
+                       [ 'b', 'd', 16],
+                       [ 'c', 'd', 20]]
     
     #DEBUG print("Edge Weights:", edgeWeights)
         
-    Dijkstra()
+    Dijkstra('c')
     
 
 # The 0th argument is the file name
