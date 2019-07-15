26e830566c0e34640e59fcc771e70c0a5bae8f0a
Catherine Halsey <stisbi@gmail.com>
2019-07-14 20:59:35 -0500

    Works for uninformed search now.

--- Python/src/AIAlgorithms/CityRoutes.py
+++ Python/src/AIAlgorithms/CityRoutes.py
@@ -3,26 +3,30 @@ from CommonUse import ReadFromFile as rff
 import os
 import sys
 
-class RouteData():
-    fromCity = ""
-    toCity   = ""
-    distance = 0
+class City():
+    cityName = ""
+    goesTo   = ""
     
-    def __init__(self, fromCity = "", toCity = "", distance = 0):
-        self.fromCity = fromCity
-        self.toCity   = toCity
-        self.distance = distance
+    
+    def __init__(self, cityName = "", goesTo = ""):
+        self.cityName = cityName
+        self.goesTo   = goesTo
 
 
 class CityRoutes():
     util = rff.ReadFile()
     
-    routeData = RouteData()
+    pathFound = False
+    totalDistance = 0 
+    
+    fromCity = ""
+    toCity   = ""
     
     pathToFile = ""
     
-    routeMap = {}
-    routes   = []
+    routeMap  = {}
+    routes    = []
+    cityPaths = []
     
     def __init__(self, argList):
         self.ParseArgs(argList)
@@ -31,9 +35,9 @@ class CityRoutes():
     def ParseArgs(self, argList):
         for index, arg in enumerate(argList):
             if arg == "-f":
-                self.routeData.fromCity = argList[index + 1]
+                self.fromCity = argList[index + 1]
             elif arg == "-t":
-                self.routeData.toCity = argList[index + 1]
+                self.toCity = argList[index + 1]
             elif arg == "-p":
                 self.pathToFile =  argList[index + 1]
             else:
@@ -76,12 +80,35 @@ class CityRoutes():
         print()
     
     
+    def PrintCities(self):
+        for cities in self.cityPaths:
+            print(cities)
+    
+    
+    def AddCityToList(self, cityName, goesTo):
+        if not self.cityPaths:
+            self.cityPaths.append([cityName, goesTo])
+        else:
+            for cities in self.cityPaths:
+                if cities[len(cities) - 1] == cityName:
+                    newPath = cities
+                    newPath.append(goesTo)
+                    self.cityPaths.append(newPath)
+                    break
+                elif cities[len(cities) - 2] == cityName:
+                    newPath = cities[:-1]
+                    newPath.append(goesTo)
+                    self.cityPaths.append(newPath)
+                    break
+        
+    
+    
     def CheckForRoutes(self):
-        self.CheckRoutes([self.routeData.fromCity])
+        self.CheckRoutes([self.fromCity])
     
     
     def CheckRoutes(self, currentCities):
-        print("Checking for route from " + self.routeData.fromCity + " to " + self.routeData.toCity + " in " + str(currentCities))
+        #print("Checking for route from " + self.fromCity + " to " + self.toCity + " in " + str(currentCities))
         
         newCities = []
         
@@ -89,19 +116,97 @@ class CityRoutes():
             if not self.routeMap[key]:
                 if key[0] in currentCities:
                     newCities.append(key[1])
+                    
                     self.routeMap[key] = True
+                    
+                    self.AddCityToList(key[0], key[1])
+                    
+                    
                 elif key[1] in currentCities:
                     newCities.append(key[0])
+                    
                     self.routeMap[key] = True
+                    
+                    self.AddCityToList(key[1], key[0])
         
-        if self.routeData.toCity in newCities:
-            print(newCities)
-        else:
+        if not newCities:
+            self.pathFound = False
+        elif self.toCity not in newCities:
             self.CheckRoutes(newCities)
+        else:
+            self.pathFound = True
+    
+    
+    def GetRoute(self):
+        print("Route: ")
         
+        for cities in self.cityPaths:
+            if cities[0] == self.fromCity and cities[len(cities) - 1] == self.toCity:
+                for index, path in enumerate(cities[:-1]):
+                    for route in self.routes:
+                        if cities[index] in route and cities[index + 1] in route:
+                            print(cities[index] + " to " + cities[index + 1] + ": " + route[2] + " km")
+                            self.totalDistance += int(route[2])
+                            
+        if not self.pathFound:
+            print("none")
+    
+    def GetRouteDistance(self):
+        distance = 0
+        
+        for cities in self.cityPaths:
+            if cities[0] == self.fromCity and cities[len(cities) - 1] == self.toCity:
+                for index, path in enumerate(cities[:-1]):
+                    for route in self.routes:
+                        if cities[index] in route and cities[index + 1] in route:
+                            distance += int(route[2])
+                            
+        print("Distance: ", end='', flush=True)
+    
+        if self.pathFound:
+            print(str(distance))
+        else:
+            print("infinity")
+    
+    
+    
+    def GetNodesExpanded(self):
+        print("Nodes Expanded: ", end='', flush=True)
+    
+        if self.pathFound:
+            print(str(len(self.cityPaths) + 1))
+        else:
+            print()
             
+    
+    def GetMaxMemoryNodes(self):
+        print("Max nodes in memory: ", end='', flush=True)
+    
+        if self.pathFound:
+            print(str(len(self.cityPaths) + 1))
+        else:
+            print()        
+            
+            
+    def GetNodesGenerated(self):
+        print("Nodes Generated: ", end = '', flush=True)
+        
+        if self.pathFound:
+            print(str(len(self.cityPaths) + 1))
+        else:
+            print()
+    
+    
+    def PrintInfo(self):
+        self.GetNodesExpanded()
+        self.GetNodesGenerated()
+        self.GetMaxMemoryNodes()
+        self.GetRouteDistance()
+        self.GetRoute()
+        
+        
     def Run(self):
-        if (self.routeData.fromCity == "") or (self.routeData.toCity == "") or (self.pathToFile == "") or not (os.path.isfile(self.pathToFile)):
+        if (self.fromCity == "") or (self.toCity == "") or (self.pathToFile == "") or not (os.path.isfile(self.pathToFile)):
             self.ShowHelp()
         else:
             self.GetRoutesFromFile()
@@ -115,9 +220,9 @@ def main(argv):
     if argv:
         routes = CityRoutes(argv)
         routes.Run()
-        routes.PrintRoutes()
-        routes.PrintRouteMap()
+        #routes.PrintRoutes()
         routes.CheckForRoutes()
+        routes.PrintInfo()
     else:
         print("No argument given.")
         
